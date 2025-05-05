465990f9d453488cbb4bcf0c28815f75072d7ba8
EBaBoBa481 <102860260+EBaBoBa537@users.noreply.github.com>
2025-05-05 08:25:25 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,29 @@
+// Очистка и загрузка
+localStorage.clear();
+Promise.all([
+    fetch('admin.json').then(res => res.json()),
+    fetch('films.json').then(res => res.json()),
+    fetch('days.json').then(res => res.json()),
+    fetch('seanses.json').then(res => res.json()),
+    fetch('tickets.json').then(res => res.json()),
+    fetch('users.json').then(res => res.json())
+])
+.then(([admin, films, days, seanses, tickets, users]) => {
+    localStorage.setItem('admin', JSON.stringify(admin.admin));
+    localStorage.setItem('films', JSON.stringify(films.films));
+    localStorage.setItem('days', JSON.stringify(days.days));
+    localStorage.setItem('seanses', JSON.stringify(seanses.seanses));
+    localStorage.setItem('tickets', JSON.stringify(tickets.tickets));
+    localStorage.setItem('users', JSON.stringify(users.users));
+})
+.catch(err => console.error('Ошибка при загрузке данных:', err));
+
+// ПРОВЕРКА LOCALSTORAGE
+for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    console.log(`${key}:`, JSON.parse(localStorage.getItem(key)));
+}
+
 document.querySelector('button').addEventListener('click', () => {
     const email = document.querySelector('input[name="admin-kod"]').value.trim();
     const password = document.querySelector('input[name="password"]').value.trim();
@@ -7,21 +33,13 @@ document.querySelector('button').addEventListener('click', () => {
         return;
     }
 
-    fetch('users.json')
-        .then(response => response.json())
-        .then(data => {
-            const user = data.users.find(u => u.email === email && u.password === password);
-            if (user) {
-                localStorage.setItem('currentUser', JSON.stringify(user));
-                alert('Вы успешно вошли!');
-                // Переход на главную страницу после входа
-                window.location.href = 'home.html';
-            } else {
-                alert('Неверный email или пароль.');
-            }
-        })
-        .catch(error => {
-            console.error('Ошибка при загрузке users.json:', error);
-            alert('Ошибка при авторизации. Попробуйте позже.');
-        });
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const user = users.find(u => u.email === email && u.password === password);
+    if (user) {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        alert('Вы успешно вошли!');
+        window.location.href = 'home.html';
+    } else {
+        alert('Неверный email или пароль.');
+    }
 });
